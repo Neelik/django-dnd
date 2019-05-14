fab1a586054ed9a5d9fd4fe23b8d7defb81197a0
Alex Esplin <alexr.esplin@gmail.com>
2019-05-14 10:51:29 -0600

    Added character model

--- apps/fifth_edition/models.py
+++ apps/fifth_edition/models.py
@@ -20,21 +20,37 @@ class Alignment(models.Model):
         verbose_name_plural = "Alignments"
 
 
-class Skill(models.Model):
+class Character(models.Model):
     """
-    Model storing the information about the respective skills a character possesses.
-
-    All descriptions pulled from http://5e.d20srd.org/indexes/skills.htm
-
+    Model to store information about a character in DnD 5e
     """
-    name = models.CharField(max_length=16)
-    description = models.TextField()
+
+    RACE_CHOICES = (
+        ("Dragonborn", "Dragonborn"),
+        ("Dwarf", "Dwarf"),
+        ("Elf", "Elf"),
+        ("Gnome", "Gnome"),
+        ("Half Elf", "Half Elf"),
+        ("Half Orc", "Half Orc"),
+        ("Halfling", "Halfling"),
+        ("Human", "Human"),
+        ("Tiefling", "Tiefling")
+    )
+
+    name = models.CharField(max_length=50, verbose_name='Character Name')
+    level = models.IntegerField(default=1, verbose_name='Level')
+    character_class = models.CharField(max_length=20, verbose_name='Class')
+    background = models.TextField(verbose_name='Background', null=True, blank=True)
+    player_name = models.CharField(max_length=50, verbose_name='Players Name')
+    race = models.CharField(max_length=20, choices=RACE_CHOICES, verbose_name='Race')
+    alignment = models.CharField(max_length=50, verbose_name='Alignment')
+    experience_points = models.PositiveIntegerField(default=0, verbose_name='Experience Points')
 
     def __str__(self):
         return self.name.capitalize()
 
     class Meta:
-        verbose_name_plural = "Skills"
+        unique_together = ("name", "character_class", "race", "player_name")
 
 
 class Language(models.Model):
