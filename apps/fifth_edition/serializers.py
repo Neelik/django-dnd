8ce92fa4493f94567d69a1342a68bf4217f817b7
jncwinner <43044912+jncwinner@users.noreply.github.com>
2019-05-16 17:39:10 -0600

    Transfer Ability Score Model #3 (#14)
    finish #3

--- apps/fifth_edition/serializers.py
+++ apps/fifth_edition/serializers.py
@@ -10,3 +10,21 @@ class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Character
         fields = "__all__"
+
+
+class AbilityScoreSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the AbilityScore model
+    """
+
+    class Meta:
+        model = models.AbilityScore
+        fields = ("id",
+                  "strength", "strength_modifier",
+                  "dexterity", "dexterity_modifier",
+                  "constitution", "constitution_modifier",
+                  "intelligence", "intelligence_modifier",
+                  "wisdom", "wisdom_modifier",
+                  "charisma", "charisma_modifier")
+        read_only_fields = ("strength_modifier", "dexterity_modifier", "constitution_modifier", "intelligence_modifier",
+                            "wisdom_modifier", "charisma_modifier")
