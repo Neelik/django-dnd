a326e96f8a86e1b567f060632cfca676a6a76ecd
Garrett Allen <neelik.n33l@gmail.com>
2019-05-17 20:30:13 -0600

    Transfer of Skills and Spellcasting models. (#16)
    Closes #7

--- apps/fifth_edition/serializers.py
+++ apps/fifth_edition/serializers.py
@@ -2,6 +2,27 @@ from apps.fifth_edition import models
 from rest_framework import serializers
 
 
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
 class CharacterSerializer(serializers.ModelSerializer):
     """
     Serializer class for the Character model
@@ -39,3 +60,29 @@ class PhysicalAttackSerializer(serializers.ModelSerializer):
         model = models.PhysicalAttack
         fields = ("id", "ability_score", "name", "damage_type", "dice_type", "dice_count", "str_atk_bonus", "dex_atk_bonus")
         read_only_fields = ("str_atk_bonus", "dex_atk_bonus")
+
+
+class SkillsSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the Skills model
+    """
+
+    class Meta:
+        model = models.Skills
+        fields = ("id", "acrobatics", "animal_handling", "arcana", "athletics", "deception", "history", "insight",
+                  "intimidation", "investigation", "medicine", "nature", "perception", "performance", "persuasion",
+                  "religion", "sleight_of_hand", "stealth", "survival", "ability_score")
+        read_only_fields = ("acrobatics", "animal_handling", "arcana", "athletics", "deception", "history", "insight",
+                  "intimidation", "investigation", "medicine", "nature", "perception", "performance", "persuasion",
+                  "religion", "sleight_of_hand", "stealth", "survival")
+
+
+class SpellcastingSerializer(DynamicFieldsModelSerializer):
+    """
+    Serializer class for Spellcasting model
+    """
+
+    class Meta:
+        model = models.Spellcasting
+        fields = ("id", "ability_score", "spellcasting_ability", "spell_attack", "spell_save")
+        read_only_fields = ("spell_attack", "spell_save")
