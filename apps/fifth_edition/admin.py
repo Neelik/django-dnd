76a5983648e00863ba4713a6b5a6d056fbf4be76
Mike Martin <mmartin@relias.com>
2019-05-21 18:41:44 -0600

    Added code in admin.py

--- apps/fifth_edition/admin.py
+++ apps/fifth_edition/admin.py
@@ -2,6 +2,11 @@ from apps.fifth_edition import models as models
 from django.contrib import admin
 
 
+class BackgroundAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+    search_fields = ("name", "spec", "feature", "alt_feature", "traits", "ideals", "bonds", "flaws", "equipment")
+
+
 class CharacterAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "level", "character_class", "race")
     search_fields = ("name", "level", "character_class", "race", "player_name")
@@ -11,6 +16,10 @@ class CharacterAdmin(admin.ModelAdmin):
 class PhysicalAttackAdmin(admin.ModelAdmin):
     list_display = ("id", "name", 'damage_type')
     search_fields = ("name", "damage_type")
+
+
+class SaveAdmin(admin.ModelAdmin):
+    list_display = ("id", "ability_score")
 
 
 class SkillsAdmin(admin.ModelAdmin):
@@ -23,7 +32,9 @@ class SpellcastingAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
+admin.site.register(models.Background, BackgroundAdmin)
 admin.site.register(models.Character, CharacterAdmin)
 admin.site.register(models.PhysicalAttack, PhysicalAttackAdmin)
+admin.site.register(models.Save, SaveAdmin)
 admin.site.register(models.Skills, SkillsAdmin)
 admin.site.register(models.Spellcasting, SpellcastingAdmin)
